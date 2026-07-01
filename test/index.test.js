2647b7d1f232f1fc8ee316957df1041947c20be0
swapneel1026 <swapneelshubham44@gmail.com>
2026-07-02 01:53:51 +0530

    feat: add multiply function and corresponding tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,9 @@
-const { add } = require('../index');
+const { add , multiply } = require('../index');
 
 test('adds 2 + 3', () => {
     expect(add(2, 3)).toBe(5);
+});
+
+test('multiply 2 * 3', () => {
+    expect(multiply(2, 3)).toBe(6);
 });
